b7fc34d0ef9fd1dffacc03d71567ec6b38742b6b
kdipto1 <kdipto@outlook.com>
2023-06-17 23:15:33 +0600

    user interface and schema,model created

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import { IUser, UserModel } from "./user.interface";
+
+const userSchema = new mongoose.Schema<IUser, UserModel>(
+  {
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["seller", "buyer"],
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    budget: {
+      type: Number,
+      required: true,
+    },
+    income: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+export const User = mongoose.model<IUser, UserModel>("User", userSchema);
