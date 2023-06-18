a772b8aea72afd6ae0835bff0daee81a1e979deb
kdipto1 <kdipto@outlook.com>
2023-06-18 11:44:00 +0600

    Model and Interface fow [Cow] created

---
+++ src/app/modules/cow/cow.interface.ts
@@ -0,0 +1,30 @@
+import { Model, Schema } from "mongoose";
+
+export type ICOw = {
+  name: string;
+  age: number;
+  price: number;
+  location:
+    | "Dhaka"
+    | "Chattogram"
+    | "Barishal"
+    | "Rajshahi"
+    | "Sylhet"
+    | "Comilla"
+    | "Rangpur"
+    | "Mymensingh";
+  breed:
+    | "Brahman"
+    | "Nellore"
+    | "Sahiwal"
+    | "Gir"
+    | "Indigenous"
+    | "Tharparkar"
+    | "Kankrej";
+  weight: number;
+  label: "for sale" | "sold out";
+  category: "Dairy" | "Beef" | "Dual Purpose";
+  seller: Schema.Types.ObjectId;
+};
+
+export type CowModel = Model<ICOw, Record<string, unknown>>;
---
+++ src/app/modules/cow/cow.model.ts
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+const cowSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    location: {
+      type: String,
+      enum: [
+        "Dhaka",
+        "Chattogram",
+        "Barishal",
+        "Rajshahi",
+        "Sylhet",
+        "Comilla",
+        "Rangpur",
+        "Mymensingh",
+      ],
+      required: true,
+    },
+    breed: {
+      type: String,
+      enum: [
+        "Brahman",
+        "Nellore",
+        "Sahiwal",
+        "Gir",
+        "Indigenous",
+        "Tharparkar",
+        "Kankrej",
+      ],
+      required: true,
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    label: {
+      type: String,
+      enum: ["for sale", "sold out"],
+      default: "for sale",
+    },
+    category: {
+      type: String,
+      enum: ["Dairy", "Beef", "Dual Purpose"],
+      required: true,
+    },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+export const Cow = mongoose.model("Cow", cowSchema);
