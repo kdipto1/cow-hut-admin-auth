d516696aa7e5ac021c5f9903d17c855abf2ac76d
kdipto1 <kdipto@outlook.com>
2023-07-01 23:36:31 +0600

    [User] single user get,patch,delete and all user get route guarded with admin role

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,14 +1,28 @@
 import express from "express";
 import { UserController } from "./user.controller";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enums/roles";
 
 const router = express.Router();
 
 router.post("/auth/signup", UserController.createUser);
 router.post("/auth/login", UserController.loginUser);
 router.post("/auth/refresh-token", UserController.refreshToken);
-router.get("/users/:id", UserController.getSingleUser);
-router.patch("/users/:id", UserController.updateUser);
-router.delete("/users/:id", UserController.deleteUser);
-router.get("/users", UserController.getAllUsers);
+router.get(
+  "/users/:id",
+  auth(ENUM_USER_ROLE.ADMIN),
+  UserController.getSingleUser
+);
+router.patch(
+  "/users/:id",
+  auth(ENUM_USER_ROLE.ADMIN),
+  UserController.updateUser
+);
+router.delete(
+  "/users/:id",
+  auth(ENUM_USER_ROLE.ADMIN),
+  UserController.deleteUser
+);
+router.get("/users", auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
 
 export const UserRoutes = router;
