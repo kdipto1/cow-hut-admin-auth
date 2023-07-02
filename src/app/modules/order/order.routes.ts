bea30a58a293076957f863c0ca2ecfa9eddcc658
kdipto1 <kdipto@outlook.com>
2023-07-02 14:00:19 +0600

    [Order]- create/post new order route guarded, only buyer access available

--- src/app/modules/order/order.routes.ts
+++ src/app/modules/order/order.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { OrderController } from "./order.controller";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enums/roles";
 
 const router = express.Router();
 
-router.post("/", OrderController.createOrder);
+router.post("/", auth(ENUM_USER_ROLE.BUYER), OrderController.createOrder);
 router.get("/", OrderController.getAllOrders);
 
 export const OrderRoutes = router;
